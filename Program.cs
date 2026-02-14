3d943f1d37456ac5ffb486987e5f8f203e5c8c8f
Mohsen Abbasi <mabbasidesign@yahoo.com>
2026-02-14 11:07:03 -0500

    Add Middleware folder for future custom middleware organization

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
+
 using LaunchPad.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10, // 10 requests
+                Window = TimeSpan.FromSeconds(10), // per 10 seconds
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 2
+            }
+        ));
+    options.RejectionStatusCode = 429;
+});
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -12,7 +31,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+// Use rate limiting middleware
+app.UseRateLimiter();
 
 
 // Configure the HTTP request pipeline.
